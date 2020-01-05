2afcb705f7c38a12127af6997ae2d660a93ff700
Renato Groffe <renato.groffe@yahoo.com.br>
2020-01-05 20:23:00 -0300

    ASP.NET Core 3.0 + Azure Service Bus + API REST
    ASP.NET Core 3.0 + Azure Service Bus + API REST

---
+++ APIMensagens/Controllers/MensagensController.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Azure.ServiceBus;
+using APIMensagens.Models;
+
+namespace APIMensagens.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MensagensController : ControllerBase
+    {
+        [HttpPost]
+        public object Post(
+            [FromServices]IConfiguration config,
+            Mensagem mensagem)
+        {
+            var body = Encoding.UTF8.GetBytes(mensagem.Conteudo);
+
+            var client = new QueueClient(
+                config["AzureServiceBus:ConnectionString"],
+                config["AzureServiceBus:Queue"],
+                ReceiveMode.ReceiveAndDelete);
+            client.SendAsync(new Message(body)).Wait();
+
+            return new
+            {
+                Resultado = "Mensagem processada com sucesso!"
+            };
+        }
+    }
+}
---
+++ APIMensagens/Models/Mensagem.cs
@@ -0,0 +1,7 @@
+namespace APIMensagens.Models
+{
+    public class Mensagem
+    {
+        public string Conteudo { get; set; }
+    }
+}
